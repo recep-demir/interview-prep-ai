863a004fa3a96747f8211614dcd5a1934d88fd99
Recep Demir <demir.rp@gmail.com>
2025-09-27 22:29:13 +0200

    Refactor App component to properly structure routing and integrate Toaster component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,8 +17,24 @@ const App = () => {
     <UserProvider>
       <div>
         <Router>
-          
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+              path="/interview-prep/:sessionId"
+              element={<InterviewPrep />}
+            />
+          </Routes>
+
         </Router>
+         <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
 
       </div>
       </UserProvider>
