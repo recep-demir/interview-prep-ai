50da6243fb0cad1de3185b3fe247b4a5b267c241
Recep Demir <demir.rp@gmail.com>
2025-09-27 22:31:46 +0200

    Remove unused imports for Login and SignUp components in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import LandingPage from "./pages/LandingPage";
-import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/SignUp";
 import Dashboard from "./pages/Home/Dashboard";
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 import { UserProvider } from "./context/userContext";
-
-
 
 
 const App = () => {
