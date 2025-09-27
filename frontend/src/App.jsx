0316b2c15960d0eb6b1063059fc3c330a926d965
Recep Demir <demir.rp@gmail.com>
2025-09-27 22:32:06 +0200

    Remove unnecessary blank lines in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Home/Dashboard";
 import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
@@ -31,7 +30,6 @@ const App = () => {
             },
           }}
         />
-
       </div>
       </UserProvider>
   )
