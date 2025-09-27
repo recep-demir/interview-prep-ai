e4851a294baf4c06576c0dc21c120c8c1cb6d09d
Recep Demir <demir.rp@gmail.com>
2025-09-27 22:24:19 +0200

    Add routing and page imports to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+import { UserProvider } from "./context/userContext";
+
+
+
 
 const App = () => {
   return (
