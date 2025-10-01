444dc5c1c595c60e63461e96e42884b5cbbd1703
Recep Demir <demir.rp@gmail.com>
2025-10-01 22:16:49 +0200

    Fix import path for InterviewPrep component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Home/Dashboard";
-import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+import InterviewPrep from "./pages/InterviewPrep/components/InterviewPrep";
 // import { UserProvider } from "./context/userContext";
 
 
