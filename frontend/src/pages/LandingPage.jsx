d40f0ba77ee468499c6c0a235bfeced259132b61
Recep Demir <demir.rp@gmail.com>
2025-10-01 22:33:54 +0200

    Add imports for state management and routing in LandingPage component

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from "react";
+import {APP_FEATURES} from "../utils/data";
+import { useNavigate } from "react-router-dom";
+import { LuSparkles } from "react-icons/lu";
 
 const LandingPage = () => {
   return (
