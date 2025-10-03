d49ac467bf5b5533833fd71580762c30d08ca794
Recep Demir <demir.rp@gmail.com>
2025-10-03 15:01:30 +0200

    Refactor UserContext: Improve code readability by adding comments and formatting

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,13 +1,15 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
-const UserContext = createContext(null);
+// Context oluştur
+export const UserContext = createContext(null);
 
-export const UseProvider = ({ children}) => {
-    const [user, setUser] = useState(null);
+// Provider component
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
-}
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
