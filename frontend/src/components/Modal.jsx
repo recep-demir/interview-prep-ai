11a2d346a2a442062321b5bade1430cf5b8de014
Recep Demir <demir.rp@gmail.com>
2025-10-06 21:52:47 +0200

    Refactor Modal and LandingPage: Enhance modal structure and improve landing page layout for better user experience

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const Modal = () => {
+const Modal = ({ children, isOpen, onClose, title, hideHeader }) => {
+  if (!isOpen) return null; // modal kapalıysa hiç render edilmez
+
   return (
-    <div>Modal</div>
-  )
-}
+    <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40">
+      {/* Modal Box */}
+      <div className="relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden">
+        
+        {/* Header */}
+        {!hideHeader && (
+          <div className="flex items-center justify-between p-4 border-b border-gray-200">
+            <h3 className="md:text-lg font-medium text-gray-900">{title}</h3>
+          </div>
+        )}
 
-export default Modal
+        {/* Close Button */}
+        <button
+          type="button"
+          className="text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex justify-center items-center absolute top-3.5 right-3.5 cursor-pointer"
+          onClick={onClose}
+        >
+          <svg
+            className="w-3 h-3"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+
+        {/* Modal Body (scrollable if content is large) */}
+        <div className="flex-1 overflow-y-auto custom-scrollbar p-4">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
