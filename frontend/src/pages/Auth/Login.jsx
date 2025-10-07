facb38fb24d2b3ad3e2e5620ed317d3db234c9df
Recep Demir <demir.rp@gmail.com>
2025-10-07 21:35:41 +0200

    Refactor Login component: Implement state management for email, password, and error handling

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { UserContext } from "../../context/userContext";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
 
-export default Login
+const handleLogin = async (e) => {
+  e.preventDefault();
+
+  if (!validateEmail(email)) {
+    setError("Please enter a valid email address.");
+    return;
+  }
+
+  if (!password) {
+    setError("Please enter the password");
+    return;
+  }
+
+  setError("");
+
+  
