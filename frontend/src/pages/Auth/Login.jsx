1d9f75ee77f97503134fbaf9daadda2579b7cb11
Recep Demir <demir.rp@gmail.com>
2025-10-09 22:53:00 +0200

    Refactor Login component: Enhance error handling and improve code structure for better readability

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -12,22 +12,90 @@ const Login = ({ setCurrentPage }) => {
   const [error, setError] = useState(null);
 
   const { updateUser } = useContext(UserContext);
-
   const navigate = useNavigate();
 
-const handleLogin = async (e) => {
-  e.preventDefault();
+  // Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-  if (!validateEmail(email)) {
-    setError("Please enter a valid email address.");
-    return;
-  }
+    // basic validation
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
 
-  if (!password) {
-    setError("Please enter the password");
-    return;
-  }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
 
-  setError("");
+    setError("");
 
-  export default Login;
+    try {
+      // Login API Call
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+        />
+
+        <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 8 Characters"
+          type="password"
+        />
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+        <button type="submit" className="btn-primary">
+          LOGIN
+        </button>
+
+        <p className="text-[13px] text-slate-800 mt-3">
+          Don’t have an account?{" "}
+          <button
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => setCurrentPage("signup")}
+          >
+            SignUp
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
