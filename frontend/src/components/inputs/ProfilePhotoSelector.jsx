2a1e6365d4434c000939354dcee937614c8f339f
Recep Demir <demir.rp@gmail.com>
2025-10-10 15:50:49 +0200

    Add ProfilePhotoSelector component: Implement file selection, preview, and removal functionality

--- frontend/src/components/inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/inputs/ProfilePhotoSelector.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { LuUser, LuUpload, LuTrash } from "react-icons/lu";
 
-const ProfilePhotoSelector = () => {
+const ProfilePhotoSelector = ({ image, setImage, preview, setPreview }) => {
+  const inputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  // When the user selects a file
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // Save the file in parent component
+      setImage(file);
+
+      // Create preview URL
+      const preview = URL.createObjectURL(file);
+      if (setPreview) setPreview(preview);
+      setPreviewUrl(preview);
+    }
+  };
+
+  // Remove image and reset
+  const handleRemoveImage = () => {
+    setImage(null);
+    setPreviewUrl(null);
+    if (setPreview) setPreview(null);
+  };
+
+  // Click hidden input field
+  const onChooseFile = () => {
+    inputRef.current.click();
+  };
+
   return (
-    <div>ProfilePhotoSelector</div>
-  )
-}
+    <div className="flex justify-center mb-6">
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
 
-export default ProfilePhotoSelector
+      {/* If no image selected */}
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center bg-orange-50 rounded-full relative cursor-pointer">
+          <LuUser className="text-4xl text-orange-500" />
+
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-linear-to-r from-orange-500/85 to-orange-600 text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer"
+            onClick={onChooseFile}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        // If image is selected
+        <div className="relative">
+          <img
+            src={preview || previewUrl}
+            alt="profile photo"
+            className="w-20 h-20 rounded-full object-cover"
+          />
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1 cursor-pointer"
+            onClick={handleRemoveImage}
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfilePhotoSelector;
