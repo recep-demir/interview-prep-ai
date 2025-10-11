98af70956e4dc8d0876101988d209a5d0eb5151b
Recep Demir <demir.rp@gmail.com>
2025-10-11 21:55:02 +0200

    Add Input component: Implement show/hide password functionality with state management

--- frontend/src/components/inputs/Input.jsx
+++ frontend/src/components/inputs/Input.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
+const Input = ({ value, onChange, label, placeholder, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  
 const Input = () => {
   return (
     <div>Input</div>
