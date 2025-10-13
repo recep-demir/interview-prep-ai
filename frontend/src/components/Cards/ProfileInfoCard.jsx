16ee313d277446116ca49422856757689fea0f20
Recep Demir <demir.rp@gmail.com>
2025-10-13 22:30:35 +0200

    Refactor ProfileInfoCard: Improve logout functionality and user display logic

--- frontend/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/src/components/Cards/ProfileInfoCard.jsx
@@ -2,3 +2,33 @@ import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 
+const { user, clearUser } = useContext(UserContext);
+
+const handelLogout = () => {
+  localStorage.clear(); 
+  clearUser();           
+  navigate("/");         
+};
+
+return (
+  user && (   // sadece kullanıcı varsa göster
+    <div className="flex items-center">
+      <img
+        src={user.profileImageUrl}
+        alt=""
+        className="w-11 h-11 bg-gray-300 rounded-full mr-3"
+      />
+      <div>
+        <div className="text-[15px] text-black font-bold leading-3">
+          {user.name || ""}
+        </div>
+        <button
+          className="text-amber-600 text-sm font-semibold cursor-pointer hover:underline"
+          onClick={handelLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  )
+);
