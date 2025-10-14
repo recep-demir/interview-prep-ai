bd518530b96aa81ad62d5f72a53b3eae6a00a36a
Recep Demir <demir.rp@gmail.com>
2025-10-14 21:49:27 +0200

    Refactor Dashboard: Add necessary imports for functionality and layout

--- frontend/src/pages/Home/Dashboard.jsx
+++ frontend/src/pages/Home/Dashboard.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { LuPlus } from "react-icons/lu";
+import { CARD_BG } from "../../utils/data";
+import toast from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import SummaryCard from "../../components/Cards/SummaryCard";
+import moment from "moment";
+import Modal from "../../components/Modal";
+import CreateSessionForm from "./CreateSessionForm";
+import DeleteAlertContent from "../../components/DeleteAlertContent";
 
 const Dashboard = () => {
   return (
