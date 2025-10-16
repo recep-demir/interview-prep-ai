40fdb1d4c3ee84ef18aefdb8356a45ff0457f82e
Recep Demir <demir.rp@gmail.com>
2025-10-16 22:03:02 +0200

    Refactor InterviewPrep component: Add necessary imports for state management, routing, animations, and UI components

--- frontend/src/pages/InterviewPrep/components/InterviewPrep.jsx
+++ frontend/src/pages/InterviewPrep/components/InterviewPrep.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import moment from "moment";
+import { AnimatePresence, motion } from "framer-motion";
+import { LuCircleAlert, LuListCollapse } from "react-icons/lu";
+import SpinnerLoader from "../../components/Loader/SpinnerLoader";
+import { toast } from "react-hot-toast";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import RoleInfoHeader from "./components/RoleInfoHeader";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import QuestionCard from "../../components/Cards/QuestionCard";
+import Drawer from "../../components/Drawer";
+import SkeletonLoader from "../../components/Loader/SkeletonLoader";
+import AIResponsePreview from "./components/AIResponsePreview";
 
 const InterviewPrep = () => {
   return (
