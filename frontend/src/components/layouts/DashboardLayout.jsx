efd193c04320f416f0844aba8933ed6af17d63c1
Recep Demir <demir.rp@gmail.com>
2025-10-17 22:07:42 +0200

    Refactor DashboardLayout component: Integrate user context and update layout structure to conditionally render children

--- frontend/src/components/layouts/DashboardLayout.jsx
+++ frontend/src/components/layouts/DashboardLayout.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import Navbar from "./Navbar";
 
-const DashboardLayout = () => {
+const DashboardLayout = ({ children }) => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>DashboardLayout</div>
-  )
-}
+    <div>
+      {/* Üst kısım: Navbar her sayfada görünür */}
+      <Navbar />
 
-export default DashboardLayout
+      {/* Alt kısım: sayfa içeriği */}
+      {user && <div>{children}</div>}
+    </div>
+  );
+};
+
+export default DashboardLayout;
