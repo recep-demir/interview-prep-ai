c9b7099b28f10ac64c362ae0e0d87d23d364d53a
Recep Demir <demir.rp@gmail.com>
2025-10-17 22:14:19 +0200

    Refactor SpinnerLoader component: Update structure to render a spinning loader

--- frontend/src/components/loader/SpinnerLoader.jsx
+++ frontend/src/components/loader/SpinnerLoader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const SpinnerLoader = () => {
   return (
-    <div>SpinnerLoader</div>
-  )
-}
+    <div
+      className="inline-block w-4 h-4 border-2 border-t-transparent border-white rounded-full animate-spin mr-2"
+      role="status"
+    />
+  );
+};
 
-export default SpinnerLoader
+export default SpinnerLoader;
