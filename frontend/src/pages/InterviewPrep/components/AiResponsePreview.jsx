9e247e55bd35acef0ab2d834fad1f623a0a7e3ad
Recep Demir <demir.rp@gmail.com>
2025-10-17 21:58:58 +0200

    Refactor AiResponsePreview component: Add necessary imports for enhanced functionality and future features

--- frontend/src/pages/InterviewPrep/components/AiResponsePreview.jsx
+++ frontend/src/pages/InterviewPrep/components/AiResponsePreview.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from "react";
+import { LuCopy, LuCheck, LuCode } from "react-icons/lu";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const AiResponsePreview = () => {
   return (
