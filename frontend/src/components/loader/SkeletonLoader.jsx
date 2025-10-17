f556b1786281c0dac461b309e8680b2a042311ee
Recep Demir <demir.rp@gmail.com>
2025-10-17 22:11:01 +0200

    Refactor Navbar and SkeletonLoader components: Implement initial structure and styling for Navbar and SkeletonLoader

--- frontend/src/components/loader/SkeletonLoader.jsx
+++ frontend/src/components/loader/SkeletonLoader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const SkeletonLoader = () => {
   return (
-    <div>SkeletonLoader</div>
-  )
-}
+    <div className="animate-pulse">
+      {/* Başlık kısmı */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 mb-4"></div>
 
-export default SkeletonLoader
+      {/* Paragraf kısmı */}
+      <div className="space-y-3">
+        <div className="h-3 bg-gray-200 rounded w-full"></div>
+        <div className="h-3 bg-gray-200 rounded w-11/12"></div>
+        <div className="h-3 bg-gray-200 rounded w-10/12"></div>
+      </div>
+
+      {/* Alt kısım */}
+      <div className="mt-6 space-y-3">
+        <div className="h-3 bg-gray-200 rounded w-8/12"></div>
+        <div className="h-3 bg-gray-200 rounded w-9/12"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonLoader;
