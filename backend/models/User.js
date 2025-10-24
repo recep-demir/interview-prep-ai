5095c860d5966f76496d48272d7ad1d6f9529712
Recep Demir <demir.rp@gmail.com>
2025-10-24 09:09:49 +0200

    Refactor User schema for improved readability and consistency

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,22 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profileImageUrl: { type: String, default: null },
+    name: {
+        type: String,
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    profileImageUrl: { 
+        type: String, 
+        default: null },
   },
   { timestamps: true }
 );
