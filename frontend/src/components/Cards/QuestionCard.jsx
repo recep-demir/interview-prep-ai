c4d82c6aa4383a284dab47d766e359283ff468dc
Recep Demir <recep.demir@powercoders.org>
2025-10-29 23:43:19 +0100

    fix: correct casing in import path for AIResponsePreview component in QuestionCard

--- frontend/src/components/Cards/QuestionCard.jsx
+++ frontend/src/components/Cards/QuestionCard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { LuChevronDown, LuPin, LuPinOff, LuSparkles } from "react-icons/lu";
-import AIResponsePreview from "../../pages/InterviewPrep/components/AIResponsePreview";
+import AIResponsePreview from "../../pages/InterviewPrep/components/AiResponsePreview";
 
 const QuestionCard = ({
   question,
