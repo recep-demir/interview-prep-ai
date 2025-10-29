98e2dcdc18b8fec9483f296579db1306d4c59ec6
Recep Demir <recep.demir@powercoders.org>
2025-10-29 23:43:22 +0100

    fix: correct casing in import path for UserContext in DashboardLayout component

--- frontend/src/components/layouts/DashboardLayout.jsx
+++ frontend/src/components/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserContext } from "../../context/userContext";
+import { UserContext } from "../../context/UserContext";
 import Navbar from "./Navbar";
 
 const DashboardLayout = ({ children }) => {
